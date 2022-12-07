8c4ca5bb969fef92326d8aa993c19aab518d32a7
Adriana Kramer <adri.kramer@gmail.com>
2022-12-07 21:58:34 +0100

    Add part one of day 2

--- lib/day_2.rb
+++ lib/day_2.rb
@@ -14,10 +14,31 @@ class Day2
 
   def part_one 
     draws = [['A', 'X'], ['B', 'Y'], ['C', 'Z']]
-    wins = [['A', 'Y'], ['B', 'Z'], ['C', 'Z']]
+    wins = [['A', 'Y'], ['B', 'Z'], ['C', 'X']]
     loses = [['A', 'Z'], ['B', 'X'], ['C', 'Y']]
+    total_score = 0
 
-    strategy_lines = parse_input('/home/adri/Documents/Programming/Ruby/repos/Advent_of_Code_2022/inputs/day_1.txt')
-    
+    strategy_lines = parse_input('/home/adri/Documents/Programming/Ruby/repos/Advent_of_Code_2022/inputs/day_2.txt')
+    strategy_lines.each do |line|
+      arr = line.split
+      if draws.include?(arr)
+        total_score += DRAW_POINTS
+      elsif wins.include?(arr)
+        total_score += WIN_POINTS
+      end
+
+      case arr[1]
+      when 'X'
+        total_score += ROCK_POINTS
+      when 'Y'
+        total_score += PAPER_POINTS
+      else
+        total_score += SCISSORS_POINTS
+      end
+    end
+    total_score
   end
 end
+
+d2 = Day2.new
+puts d2.part_one
