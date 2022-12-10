2895dda076dad786f63fc3db749bf0212d65136f
Adriana Kramer <adri.kramer@gmail.com>
2022-12-10 07:57:47 +0100

    Add day 3 part 2

--- lib/day_3.rb
+++ lib/day_3.rb
@@ -14,33 +14,37 @@ class Day3
     inputs = parse_input('./inputs/day_3.txt')
     inputs.each do |line|
       array = line.split('')
-      half1 = array.slice(0...line.length/2)
-      half2 = array.slice(line.length/2..line.length)
+      half1 = array.slice(0...line.length / 2)
+      half2 = array.slice(line.length / 2..line.length)
       common_letter = half1.intersection(half2)
       total += common_letter[0].ord
-      if common_letter[0].ord >= 97
-        total -= SHIFT_LOWER
-      else
-        total -= SHIFT_UPPER
-      end
+      total -= if common_letter[0].ord >= 97
+                 SHIFT_LOWER
+               else
+                 SHIFT_UPPER
+               end
     end
     total
   end
 
   def part_two
     total = 0
-    inputs = parse_input('./inputs/day_3.txt')
+    #inputs = parse_input('./inputs/day_3_test.txt')
+    inputs = parse_input('/home/adri/Documents/Programming/Ruby/repos/Advent_of_Code_2022/inputs/day_3.txt')
     tuple_array = []
     inputs.each_slice(3) { |tuple| tuple_array.push(tuple) }
     tuple_array.each do |tuple|
       common_letter = tuple[0].split('').intersection(tuple[1].split('')).intersection(tuple[2].split(''))
       total += common_letter[0].ord
-      if common_letter[0].ord >= 97
-        total -= SHIFT_LOWER
-      else
-        total -= SHIFT_UPPER
-      end
+      total -= if common_letter[0].ord >= 97
+                 SHIFT_LOWER
+               else
+                 SHIFT_UPPER
+               end
     end
     total
   end
 end
+
+d = Day3.new
+puts d.part_two
